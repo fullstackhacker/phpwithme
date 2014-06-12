b64c446893b0b0898f32adbec886bc0cea05630b
kapadiamush <kapadiamush@gmail.com>
2014-06-12 08:59:46 -0400

    debugged easybake/addfieldsto and created the database object

---
+++ php_framework/models/database.php
@@ -0,0 +1,28 @@
+<?php
+  /**
+   * database
+   * Created by: Mushaheed Kapadia
+   */
+  class database {
+
+				$conn=null;
+				$type;
+				$host;
+				$port;
+				$user;
+				$pass;
+		//mark
+
+
+
+
+
+
+
+
+
+    function __construct($dbname){
+
+    }
+  }
+?>
