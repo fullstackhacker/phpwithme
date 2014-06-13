02caa53b8140e7e3130170c6c86037fb325cd694
kapadiamush <kapadiamush@gmail.com>
2014-06-13 19:00:35 -0400

    working on adding database implementation

--- php_framework/models/database.php
+++ php_framework/models/database.php
@@ -1,28 +1,32 @@
 <?php
+
   /**
    * database
    * Created by: Mushaheed Kapadia
    */
   class database {
-
-				$conn=null;
-				$type;
-				$host;
-				$port;
-				$user;
-				$pass;
+		$conn=null;
+		$type;
+		$host;
+		$port;
+		$user;
+		$pass;
 		//mark
 
+		function __construct($dbname){
+			$this->type = $_CONFIG[$dbname . '_TYPE'];
+			$this->host = $_CONFIG[$dbname . '_HOST']; 
+			$this->port = $_CONFIG[$dbname . '_PORT']; 
+			$this->user = $_CONFIG[$dbname . '_USER'];
+			$this->pass = $_CONFIG[$dbname . '_PASS']; 
+
+			if($this->type == 'MySQL'){ 
+				$this->conn = mysqli_connect();
+			} 
+		}
 
 
 
 
-
-
-
-
-    function __construct($dbname){
-
-    }
   }
 ?>
